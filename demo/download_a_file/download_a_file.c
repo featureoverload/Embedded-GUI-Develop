1e1907a4ef05855e884515e6e210502ca77e4af0
Joseph Lin <joseph.lin@aliyun.com>
2019-03-12 17:43:13 +0800

    [develop] handler the download file button click

--- demo/download_a_file/download_a_file.c
+++ demo/download_a_file/download_a_file.c
@@ -24,9 +24,44 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#define STATUS_READY       0
+#define ERR_DOWNLOAD_FAIL -1
+
+int handler_display(int *);
+int handler_submit(void);
 
 int cgiMain(){
 	int ret = 0;
+	int stat = STATUS_READY;
+
+	typedef enum Act {ACT_INIT=0, DISPLAY, SUBMIT, Invalid} Act;
+	Act act = ACT_INIT;
+	int client_act = 0;
+
+	cgiFormInteger("act", &client_act, (int)DISPLAY);
+	act = (Act)client_act;
+
+	switch(act){
+		case SUBMIT:
+			stat = handler_submit();
+			return 0;
+		case DISPLAY:
+			handler_display(&stat);
+			break;
+		case Invalid:
+		default:
+			break;
+	}
+	return ret;
+}
+
+
+int handler_display(int *stat_p){
+	int ret = 0;
+
+	if (*stat_p<0){
+		// pass
+	}
 
 	// cgiWriteEnvironment("/CHANGE/THIS/PATH/capcgi.dat");
 	cgiHeaderContentType("text/html");
@@ -64,5 +99,37 @@ int cgiMain(){
 
 	free(html);
 	fclose(fp);
+	return ret;
+}
+
+/*
+ * 下载功能目前有点儿问题，抓包看一下过程！
+ * 可能和使用 apache2 作为 server 和使用 mini_httpd 作为 server 略有不同。
+ */
+#define FILENAME "firefox.exe"
+
+int handler_submit(void){
+	int ret = ERR_DOWNLOAD_FAIL;
+
+	/*
+	char cmd[256] = {'\0'};
+	snprintf(cmd, 255, "%s", "/bin/save_file");
+	ret = system(cmd);
+	if (ret!=0){
+		// DPRINT();
+		return ret;
+	}
+	*/
+	ret = 0;
+
+	printf("Pragma: no-cache\n");
+	printf("Cache-control: no-cache\n");
+	printf("Content-type: application/octet-stream\n");
+	printf("Content-Transfer-Encoding: binary\n");
+
+	printf("Content-Disposition: attachment; filename=\"%s\"\n\n",
+		   FILENAME);
+	fflush(stdout);
+
 	return ret;
 }
